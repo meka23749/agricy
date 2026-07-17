72ef491f92249d3849ce6d1e699da5c9de0e1c15
meka23749 <meka23749@gmail.com>
2026-07-17 20:59:57 +0200

    Add basic application

---
+++ build/CMakeFiles/agricy.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/agricy.dir/src/main.cpp.o"
+  "CMakeFiles/agricy.dir/src/main.cpp.o.d"
+  "agricy"
+  "agricy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/agricy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/agricy.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for agricy.
+# This may be replaced when dependencies are built.
